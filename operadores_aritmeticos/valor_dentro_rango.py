27d13bcb06ca179e3931beb1d313d78deca3083c
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:20:13 -0500

    valor en un rango

---
+++ operadores_aritmeticos/valor_dentro_rango.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+n = int(input("Ingrese un numero n: "))
+
+if 0 <= n <= 5:
+    print(f"{n} esta en el rango de 0 y 5")
+else:
+    print(f"{n} no esta en el rango de 0 y 5")
