4acdef3a40716c8895bc643275a88a7ab9995cea
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:19:10 -0500

    operadores de comparacion

---
+++ operadores_aritmeticos/operadores_comparacion.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+a, b = int(input("Ingrese un numero: ")), int(input("Ingrese un numero: "))
+
+resultado = a == b
+print(f"Resultado: == {resultado}")
+
+resultado = a != b
+print(f"Resultado: != {resultado}")
+
+resultado = a > b
+print(f"Resultado > : {resultado}")
+
+resultado = a >= b
+print(f"Resultado >= : {resultado}")
+
+resultado = a < b
+print(f"Resultado < : {resultado}")
+
+resultado = a <= b
+print(f"Resultado <= : {resultado}")
