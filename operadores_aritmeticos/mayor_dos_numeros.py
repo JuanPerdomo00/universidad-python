f4e33d205560a484ce2473f4f8b08439cd64448c
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:21:10 -0500

    ejercicio mayor de dos numeros n > n

---
+++ operadores_aritmeticos/mayor_dos_numeros.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+n1, n2 = int(input("Ingrese un 1 numero: ")), int(input("Ingrese un 2 numero: "))
+
+if n1 > n2:
+    print(f"el numero 1 {n1} es mayor que el numero 2 {n2}")
+else:
+    print(f"numero dos -> {n2} es mayor a numero uno -> {n1}")
