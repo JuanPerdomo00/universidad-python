efa666724be6c49d5ce856ae1d850019f9059b68
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:19:29 -0500

    operadores de reasignacion

---
+++ operadores_aritmeticos/operadores_reasignacion.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+mi_var = 10
+# es lo mismo que mi_var = mi_var + n donde n es el numero a usar con su operador
+mi_var += 1  # sumar 1
+print(mi_var)
+
+# restar 1
+mi_var -= 1  # 10
+print(mi_var)
+
+# y asi mismo con el resto de operadores aritmeticos
