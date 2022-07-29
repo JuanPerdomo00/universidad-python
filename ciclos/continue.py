87ec869e8a1f47f9635599e5f14b656bcd51d454
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:25:54 -0500

    continue

---
+++ ciclos/continue.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# for i in range(1, 5):
+#    if i % 2 == 0:
+#        print(f"Numeros pres: {i}")
+#        continue
+#    print(i)
+
+for i in range(1, 6 + 1):
+    if i % 2 != 0:
+        continue
+    print(f"{i}")
