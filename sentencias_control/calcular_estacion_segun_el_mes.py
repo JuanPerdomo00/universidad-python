47c12a71a0d4235cf78645b65492db87b3242c2d
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:23:05 -0500

    ejercicio calcular estacion segun el mes

---
+++ sentencias_control/calcular_estacion_segun_el_mes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+mes = int(input("Ingresa el mes de el año: "))
+estacion = None
+
+if mes == 1 or mes == 2 or mes == 12:
+    estacion = "Invierno"
+elif mes == 3 or mes == 4 or mes == 5:
+    estacion = "Primavera"
+elif mes == 6 or mes == 7 or mes == 8:
+    estacion = "Verano"
+elif mes == 9 or mes == 10 or mes == 11:
+    estacion = "Otoño"
+else:
+    estacion = "Mes incorrecto, descubriste un mes xd"
+    print(f"Mes no existe, {estacion}")
+    exit()
+    
+print(f"Para el mes {mes} la estacion es {estacion}")
