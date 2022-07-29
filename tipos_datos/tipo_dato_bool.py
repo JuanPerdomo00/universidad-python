4acd9adaf27110ee6709c152cabc45937d560c57
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:17:16 -0500

    booleanos

---
+++ tipos_datos/tipo_dato_bool.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Nos permite saber si un valor es verdadero o falso
+mi_variable = 3 > 3
+mi_variable_falsa = False
+
+if mi_variable:
+    print("Es verdadero")
+else:
+    print("Es falso")
