f94be61a832dfdc7cdb8135de0d37ad4c8b56fb8
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:16:44 -0500

    cadenas

---
+++ tipos_datos/manejo_cadenas.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Cadena string
+musico_fav = "erick Satie".capitalize()
+obra = "Gniossienne No.1"
+
+# print("Mi musico favorito es " + musico_fav + " " + "Y mi obra fav es: " + obra)
+
+print("mi musico favorito es:", musico_fav)
+
+numero = "1"
+numero2 = "2"
+
+print("Concatenacion:", numero + numero2)
+print("Suma:", int(numero) + int(numero2))
