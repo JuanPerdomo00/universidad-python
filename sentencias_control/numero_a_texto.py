120d5074935b83d0345dca7bd911feaa389f4494
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:22:34 -0500

    ejercicio numero a texto

---
+++ sentencias_control/numero_a_texto.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+num = int(input("Ingrese el numero a convertir a texto: "))
+
+if num == 1:
+    print(f"{num} - Numero Uno")
+elif num == 2:
+    print(f"{num} - Numero Dos")
+elif num == 3:
+    print(f"{num} - Numero Tres")
+else:
+    print(f'Numero no disponible a texto')
+
