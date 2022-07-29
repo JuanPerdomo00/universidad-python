dd24e5ca5aab419242d6f0e442884b460ae7ef98
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:27:43 -0500

    ejercicio calcular impuestos

---
+++ funciones/calculadora_inpuestos.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def calcular_impuesto(pago_sin, impuesto):
+    return pago_sin + pago_sin * (impuesto / 100)
+
+
+pago_sin_impuesto = int(input("Ingrese el pago sin impuesto: "))
+impuestos = int(input("Ingrese el impuesto: "))
+
+if __name__ == "__main__":
+    calculo = calcular_impuesto(pago_sin_impuesto, impuestos)
+    print(f"Pago con impuesto es: {calculo}")
+
