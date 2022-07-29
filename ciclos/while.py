aabcbc258c54d3c8c8b9300981542af4223c09cb
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:25:02 -0500

    ciclo while

---
+++ ciclos/while.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# condicion = True
+
+# x = 0
+
+# while condicion:
+#    print(x)
+#    x += 1
+#    if x == 666 + 1:
+#        break
+# else:
+#    print("Termino ciclo")
+
+
+i = 0
+
+while i <= 10:
+    print(i)
+    i += 1
+else:
+    print("fin while")
