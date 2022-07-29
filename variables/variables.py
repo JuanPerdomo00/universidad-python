4902dc6cd412a0655f9eb22b00cbc6bc7b168607
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:18:21 -0500

    variables

---
+++ variables/variables.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+x = 10 # LITERAL 10 SE ALAMCENA EN X
+y = 2 # LITERAL 2 SE ALMACENA EN Y
+z = x + y
+print(z)
+
+# direccion de memoria
+print(id(x))
+print(id(y))
+print(id(z))
+
+
+# Ejercicio
+nombre = "jakepy"
+numero_telefono = 31122334j
+email = "jakepy@fisica.com"
+
+print('el nombre es {} el mumero de telefono es {} el email es {}'.format(nombre, numero_telefono, email))
