129710f0be6844a3a8a5f08229f92d204343aba1
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:17:03 -0500

    numeros en python

---
+++ tipos_datos/tipo_dato_numero.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from numpy import pi
+
+# tipo int (entero)
+x = 10
+print(type(x))
+
+# tipo float (decimal o flotante)
+x = pi
+print(type(x))
+
+# tipo string o cadenas de caracteres
+x = "lala"
+print(type(x))
+
+# tipo de dato bool o booleano
+x = True or False
+print(type(x))
