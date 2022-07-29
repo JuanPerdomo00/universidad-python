3a141da8d2dd13ecb33c4ea0791b5e5bd25c18fd
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:26:30 -0500

    *args

---
+++ funciones/argumentos_variables_args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def iterar_nombres(*nombre: str):
+    for nombres in nombre:
+        print(nombres)
+
+
+iterar_nombres("max planck", "jakepy", "albert", "philip", "van goh")
