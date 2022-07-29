c869e492a00999ac79a8ec68f9f421f05b333538
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:20:32 -0500

    ejercicio numero par o impar

---
+++ operadores_aritmeticos/ejercicio_par_impar.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def main():
+    n = int(input("Ingrese un numero: "))
+    if n % 2 == 0:
+        print(f"{n} es un numero par")
+    else:
+        print(f"{n} no es un numero par")
+        option = input("Quiere intentarlo de nuevo[Y/n]: ")
+        if option == "" or option == "Y" or option == "y":
+            main()
+        else:
+            print("Bye")
+
+
+if __name__ == "__main__":
+    main()
