8ceb0884bcacc24f2f37a29527364de252fdc6af
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-07-29 14:16:26 -0500

    input recibir data

---
+++ tipos_datos/input.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# entrada de datos de  el usuario
+valor = input("Escribe un mensaje: ")
+a = int(input("Escribe un numero: "))
+b = int(input("Escribe un numero: "))
+suma = a + b
+
+
+print("El mensaje es:", valor)
+print("La suma es:", suma)
+print("El programa termino")
+
+
+dia = int(input("Como estuvo tu dia (1 al 10): "))
+print("Mi dia estuvo de:", dia)
+
+
+titulo_libro = input("Titulo de el libro: ")
+autor_libro = input("Autor de el libro: ")
+
+print("{} Escrito por {}".format(titulo_libro, autor_libro))
