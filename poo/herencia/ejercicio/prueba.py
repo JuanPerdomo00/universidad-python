a71abb2ce92a3f5de21e869605904b13a79d077f
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-08-02 15:33:50 -0500

    completado

---
+++ poo/herencia/ejercicio/prueba.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from herencia import *
+
+veiculo1 = Veiculo("Rojo", 4)
+coche1 = Coche("verde", 4, 10)
+bici1 = Bicicleta("azul", 2, "Extrema")
+
+print(veiculo1)
+print(coche1)
+print(bici1)
