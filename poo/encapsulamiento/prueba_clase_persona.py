aa71efe07c82961c6783bd7f3a6380f46acceeeb
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-08-02 15:57:58 -0500

    prueba de clase usando encapsulamiento

---
+++ poo/encapsulamiento/prueba_clase_persona.py
@@ -0,0 +1,8 @@
+from metodos_Get_Set import Persona
+
+print("Creacion de objetos".center(50, "-"))
+persona1 = Persona("antonio", "santos", 30)
+persona1.mostrar()
+
+print("Eliminacion de objetos".center(50, "*"))
+del persona1
