4a45e6f93576219d05dd06ef8d877a46cd75f85b
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-08-04 16:18:47 -0500

    Clase con variable de instancia

---
+++ poo/contexto_estatico_clases/MyClass.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+class Mi_Calse:
+    var_clase = "Valor de variable de clase"
+
+    def __init__(self, variable_instancia):
+        self.variable_instancia = variable_instancia
+
+
+if __name__ == "__main__":
+    # variable de objeto
+    print(Mi_Calse.var_clase)
+
+    # crear objeto para acceder a self
+    var1 = Mi_Calse("Variable de instancia self")
+    print(var1.variable_instancia)
+
+    # despues de haber creado nuestro objeto, podemos asociar una nueva variable
+    Mi_Calse.var_clase_2 = "Nueva variable clase 2"
+
+    var2 = Mi_Calse("Variable de self diferente")
+    print(var2.var_clase)
+    print(var2.variable_instancia)
+
+    print(Mi_Calse.var_clase_2)
+    print(var2.var_clase_2) # <- el ide no la reconoce divido a que se creo en vuelo
