7c3f22ab3b500ae7b19a95b4ec4faf4476624e77
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-08-05 17:46:52 -0500

    creamaos una clase propia para manejar rarchivos en python

---
+++ manejo_archivos_python/archivos_con_whith/ManejoArchivos.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class ManejoArchivos:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def __enter__(self):
+        print("Obtenemos el recurso".center(50, "-"))
+        self.nombre = open(self.nombre, "r", encoding="utf-8")
+        return self.nombre
+
+    def __exit__(self, tipo, valor, traza_del_error):
+        print("Cerramos el recurso".center(50, "-"))
+        if self.nombre:
+            self.nombre.close()
