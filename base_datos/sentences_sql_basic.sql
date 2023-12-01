c043d5f1b1b9f589ecda30c7952a7b899bd06aa0
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2023-12-01 09:58:42 -0500

    Se modificaron las sentencias sql.

--- base_datos/sentences_sql_basic.sql
+++ base_datos/sentences_sql_basic.sql
@@ -1,11 +1,19 @@
--- SELECT * FROM persona WHERE id_persona in (1,2)
--- INSERT INTO persona(nombre, apellido, correo) VALUES('noe','ovideo','noe@email.com')
+-- Se trae toda la tabla de persona
+SELECT * FROM persona
+
+-- Se trae un congunto de consultas segun el id pasado, si no existe solo trae los que encuentre.
+SELECT * FROM persona WHERE id_persona in (1,2)
+
+-- Inserta datos a la base de datos persona. Con los campos en orden que se colocaron.
+INSERT INTO persona(nombre, apellido, correo) VALUES('noe','ovideo','noe@email.com')
 -- SELECT * FROM persona
 
--- UPDATE persona SET nombre = 'angie', apellido = 'paola', correo = 'angie@paola.com' WHERE id_persona = 2
+-- Atualiza los  campos, expecificando donde el id sea un numero existente en la tabla.
+UPDATE persona SET nombre = 'ximena', apellido = 'sanchez', correo = 'angie@paola.com' WHERE id_persona = 2
 
 -- SELECT * FROM persona
 
--- DELETE FROM persona WHERE id_persona = 2
+-- Elimina una columna donde el id es 2
+DELETE FROM persona WHERE id_persona = 2
 
 -- SELECT * FROM persona
