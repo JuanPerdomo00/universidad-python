2df5684bfc3434731e0370f9b5fa3661ab6a8a34
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2025-06-01 14:48:36 -0500

    Add: Basico tkinter

---
+++ tkinter_learnig/hola_mintotk.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import tkinter as tk
+
+
+# Creamos un objeto usando la clase tk
+ventana = tk.Tk()
+
+# Modificar el tamanio ventana (pixeles)
+
+ventana.geometry("600x400")
+
+# cambiar nombre de la ventana
+ventana.title("Nueva ventana")
+
+# Iniciamos la ventana (al final)
+ventana.mainloop()
