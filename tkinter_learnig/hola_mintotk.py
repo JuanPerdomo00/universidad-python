311458a3f6ef2a710115c484bed820fd62e1f460
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2025-06-01 15:02:46 -0500

    Add: Agregando funcion al boton

--- tkinter_learnig/hola_mintotk.py
+++ tkinter_learnig/hola_mintotk.py
@@ -16,8 +16,17 @@ ventana.title("Nueva ventana")
 # configuramos icono
 ventana.iconbitmap()
 
+
+# Creamos un metodo evento_click
+def evento_click():
+    boton1.config(text='Boton clicked')
+    print("Ejecutando del evento_click")
+    # Creamos un nuevo boton y lo mostramos 
+    boton2 = ttk.Button(ventana, text="Nuevo boton")
+    boton2.pack()
+
 # Crear un boton (widget), el objeto padre es ventana 
-boton1 = ttk.Button(ventana, text="Dar click")
+boton1 = ttk.Button(ventana, text="Dar click", command=evento_click)
 
 # utilizar el pack layout manager, para mostrar el boton 
 boton1.pack()
