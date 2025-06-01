3e07f9c9ad4fed367ee00f5ef2b44446f9f41c25
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2025-06-01 14:57:28 -0500

    Add: Boton y modulo ttk de tkinter

--- tkinter_learnig/hola_mintotk.py
+++ tkinter_learnig/hola_mintotk.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import tkinter as tk
-
+from tkinter import ttk
 
 # Creamos un objeto usando la clase tk
 ventana = tk.Tk()
@@ -12,6 +12,15 @@ ventana.geometry("600x400")
 
 # cambiar nombre de la ventana
 ventana.title("Nueva ventana")
+
+# configuramos icono
+ventana.iconbitmap()
+
+# Crear un boton (widget), el objeto padre es ventana 
+boton1 = ttk.Button(ventana, text="Dar click")
+
+# utilizar el pack layout manager, para mostrar el boton 
+boton1.pack()
 
 # Iniciamos la ventana (al final)
 ventana.mainloop()
