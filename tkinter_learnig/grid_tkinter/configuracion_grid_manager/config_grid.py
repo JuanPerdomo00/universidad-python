dadfaecdf6f197386b5359b54b62b4d9a4c7f318
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2025-06-01 15:50:42 -0500

    Add: Configuracion de posiciones botones

---
+++ tkinter_learnig/grid_tkinter/configuracion_grid_manager/config_grid.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+from tkinter import Tk, ttk
+
+
+windows = Tk()
+
+windows.title("Gird tkiner")
+windows.geometry("600x400")
+# windows.iconbitmap("../icono.ico")
+
+# Configurar el grid
+windows.rowconfigure(0, weight=1)
+windows.rowconfigure(1, weight=10)
+windows.columnconfigure(0, weight=1)
+windows.columnconfigure(1, weight=5)
+windows.columnconfigure(1, weight=1)
+windows.columnconfigure(1, weight=5)
+
+
+# Metodos de eventos
+def a_event():
+    btn1.config(text="Boton 1 presionado")
+
+
+def b_event():
+    btn2.config(text="Boton 2 presionado")
+
+
+# definimos dos botones
+btn1 = ttk.Button(windows, text="Boton 1", command=a_event)
+btn1.grid(row=0, column=0, sticky="NSWE")
+
+btn2 = ttk.Button(windows, text="Boton 2", command=b_event)
+btn2.grid(row=1, column=0, sticky="NSWE")
+
+btn3 = ttk.Button(windows, text="Boton 3")
+btn3.grid(row=0, column=1, sticky="NSWE")
+
+btn4 = ttk.Button(windows, text="Boton 4")
+btn4.grid(row=1, column=1, sticky="NSWE")
+
+
+windows.mainloop()
